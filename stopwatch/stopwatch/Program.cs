9c2f8db44181710d0a897c5f0e432c618a589239
patanrafikhan <iamkhan342@gmail.com>
2023-01-29 12:02:04 +0530

    Added : Stopwatch Program

---
+++ stopwatch/stopwatch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace stopwatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var time = StopwatchHelper.MeasureRunTime(() =>
+            {
+                for (var i = 0; i < 2; i++)
+                {
+                    using (var task = Task.Delay(1000))
+                    {
+                        task.Wait();
+                    }
+                }
+            });
+
+            Console.WriteLine("Time elapsed: " + time);
+        }
+
+        static class StopwatchHelper
+        {
+            public static TimeSpan MeasureRunTime(Action codeToRun)
+            {
+                var watch = Stopwatch.StartNew();
+
+                codeToRun();
+
+                watch.Stop();
+
+                return watch.Elapsed;
+            }
+        }
+    }
+}
