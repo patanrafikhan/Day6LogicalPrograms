5a384444a49bcfb3b6ff1b5d435660da3fabd904
patanrafikhan <iamkhan342@gmail.com>
2023-01-29 12:00:56 +0530

    Added : Reverse Enterd Number Program

---
+++ reverseprogram/reverseprogram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace reverseprogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 12345678;
+            int rmd;
+            int reverse = 0;
+            while (N!= 0)
+            {
+                rmd = N % 10;
+                reverse = reverse * 10+rmd;
+                N= N / 10;
+            }
+            Console.WriteLine(reverse);
+        }
+    }
+}
