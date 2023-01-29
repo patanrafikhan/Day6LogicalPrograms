effa3cb3b63b64a19f5a10ec89b423176acf5cce
patanrafikhan <iamkhan342@gmail.com>
2023-01-29 11:56:40 +0530

    Added : Fibonaci Program

---
+++ fibonaci/fibonaci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace fibonaci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int val1 = 0, val2 = 1, val3, i, n;
+            n = 7;
+
+            Console.WriteLine("Fibonacci series :");
+            Console.WriteLine(val1 + "" + val2 + "");
+            for (i = 2; i < n; ++i)
+            {
+                val3 = val1 + val2;
+                Console.WriteLine(val3 + "");
+                val1 = val2;
+                val2 = val3;
+            }
+        }
+    }
+}
