01222aed2aeffd5bd92746c85e97df979c9fa6b6
patanrafikhan <iamkhan342@gmail.com>
2023-01-29 11:57:49 +0530

    Added : coupon Number Program

---
+++ coupennumber/coupennumber/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace coupon
+{
+    internal class Program
+    {
+        public Program()
+        {
+            Console.WriteLine("Enter the number of distinct Coupon number you want:");
+            Console.ReadKey();
+            
+            
+        }
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var chars ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            
+            var stringChars = new char[8];
+            var random = new Random();
+            
+
+            for (int j = 0; j < n; j++)
+            {
+
+
+                for (int i = 0; i < stringChars.Length; i++)
+                {
+                    stringChars[i] = chars[random.Next(chars.Length)];
+
+                }
+            }
+
+            var String1 = new String(stringChars);
+            Console.WriteLine(String1);
+
+
+
+
+        }
+    }
+}
